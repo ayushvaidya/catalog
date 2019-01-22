c6f84fd7daf85e1d90f1114b14c8e9f8716c602c
Ayush Vaidya <ayush.everest@gmail.com>
2019-01-22 03:11:48 -0500

    precompile

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Moodboard
+config.assets.initialize_on_precompile = false
+
+module Catalog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
